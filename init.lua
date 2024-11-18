9e50033db770c11ddd01f87d55b6cba332c77fcd
yiquanfeng <yiquanfeng@qq.com>
2024-11-18 23:15:21 +0800

    use all lazyvim

--- init.lua
+++ init.lua
@@ -1,7 +1,2 @@
-require('plugins.plugin-setup')
-require('core.options')
-require('core.keymaps')
-require('plugins.lualine')
-require('plugins.nvim-tree')
-require('plugins.lsp')
-require('plugins.cmp')
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
